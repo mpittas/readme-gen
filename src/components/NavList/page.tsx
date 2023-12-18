741f32f84577f5df1ad7a7323232aed37dfc296a
mpittas <webpittas@gmail.com>
2023-12-18 20:01:01 +0200

    Add and remove from active container

--- src/components/NavList/page.tsx
+++ src/components/NavList/page.tsx
@@ -10,6 +10,10 @@ interface NavListProps {
 const NavList: React.FC<NavListProps> = ({ onSelectItem }) => {
   const [searchTerm, setSearchTerm] = useState("")
 
+  const [selectedItems, setSelectedItems] = useState(
+    listItems.map((item) => ({ ...item, selected: false }))
+  )
+
   const filteredItems = listItems.filter((item) =>
     item.text.toLowerCase().includes(searchTerm.toLowerCase())
   )
@@ -24,24 +28,63 @@ const NavList: React.FC<NavListProps> = ({ onSelectItem }) => {
         sx={{ minHeight: 46 }}
       />
       <List sx={{ gap: 1 }}>
-        {filteredItems.map((item, index) => (
-          <ListItemButton
-            variant="outlined"
-            key={index}
-            onClick={() => onSelectItem(item.content, item.text)}
-            sx={{
-              borderRadius: "sm",
-              p: 1.25,
-              gap: 0.2,
-              fontSize: "14px",
-            }}
-          >
-            <ListItemDecorator>
-              <item.IconComponent />
-            </ListItemDecorator>
-            {item.text}
-          </ListItemButton>
-        ))}
+        <div style={{ backgroundColor: "#ccc" }}>
+          <h2>Active Buttons</h2>
+          {selectedItems
+            .filter((item) => item.selected)
+            .map((item, index) => (
+              <ListItemButton
+                variant="outlined"
+                key={index}
+                onClick={() => {
+                  onSelectItem(item.content, item.text)
+                  setSelectedItems(
+                    selectedItems.map((i) =>
+                      i.text === item.text ? { ...i, selected: !i.selected } : i
+                    )
+                  )
+                }}
+                sx={{
+                  borderRadius: "sm",
+                  p: 1.25,
+                  gap: 0.2,
+                  fontSize: "14px",
+                }}
+              >
+                <ListItemDecorator>
+                  <item.IconComponent />
+                </ListItemDecorator>
+                {item.text}
+              </ListItemButton>
+            ))}
+        </div>
+        {selectedItems
+          .filter((item) => !item.selected)
+          .map((item, index) => (
+            <ListItemButton
+              variant="outlined"
+              key={index}
+              onClick={() => {
+                onSelectItem(item.content, item.text)
+                setSelectedItems(
+                  selectedItems.map((i) =>
+                    i.text === item.text ? { ...i, selected: !i.selected } : i
+                  )
+                )
+              }}
+              sx={{
+                borderRadius: "sm",
+                p: 1.25,
+                gap: 0.2,
+                fontSize: "14px",
+              }}
+            >
+              <ListItemDecorator>
+                <item.IconComponent />
+              </ListItemDecorator>
+              {item.text}
+            </ListItemButton>
+          ))}
       </List>
     </>
   )
